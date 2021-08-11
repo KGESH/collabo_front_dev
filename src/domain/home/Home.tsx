f8c570018adb5faa3be80acb179234e8c59ade0b
KGESH <baram789456@gmail.com>
2021-08-11 22:07:03 +0900

    merge you, jun

--- src/domain/home/Home.tsx
+++ src/domain/home/Home.tsx
@@ -119,35 +119,3 @@ const Home = () => {
   );
 };
 export default Home;
-
-/*const GET_USER = gql`
-  query {
-    authUser {
-      id
-      name
-      email
-    }
-  }
-`;
-
-const Home = () => {
-  const { data } = useQuery(GET_USER);
-  /!**
-   * 로그인 성공하면 로그인에 대한 상태 설정필요
-   *!/
-
-  if (data?.authUser) {
-    console.log(`id: ${data.authUser.id}`);
-    console.log(`name: ${data.authUser.name}`);
-    console.log(`email: ${data.authUser.email}`);
-  }
-
-  return (
-    <>
-      <div className='home'>
-        <h1 className='home__title'>This is Home</h1>
-      </div>
-    </>
-  );
-};
-export default Home;*/
