919f54d204987c2e7d0a1c2468cada1b16db28cf
youseonghyeon <dolla_@naver.com>
2021-08-12 13:23:49 +0900

    revice (Home, QRcheck, Mypage)

--- src/domain/home/Home.tsx
+++ src/domain/home/Home.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import 'domain/home/style/Home.css';
 
 const Home = () => {
+  const main_img: string[] = ['A', 'B', 'C', 'D', 'E'];
 
   return (
     <>
       <div className='hash_tag'>해쉬 태그</div>
-      <div className='main_img'>메인_이미지</div>
-      <div className='thingthing'>하트, 돋보기, 지도 ...</div>
+      {main_img.map((img,index) => (
+        <div key={index}>
+          <div className='main_img'>메인_이미지{img}</div>
+          <div className='thingthing'>하트, 돋보기, 지도 ...</div>
+        </div>
+      ))}
+
     </>
   );
 };
--- src/domain/mypage/MyPage.tsx
+++ src/domain/mypage/MyPage.tsx
@@ -15,20 +15,20 @@ const GET_USER = gql`
 `;
 
 const MyPage = () => {
-  const { loading, data } = useQuery(GET_USER, {
-    variables: { id: 11700 },
-  });
-
-  // dev용 QRuri, dev용 cafeInfo - DB연결 후 삭제
-  const cafeInfo: string = '스타벅스 입니다.';
-
   let db_qr_list: string[] = [];
   let item_name: string[] = [];
 
+  const { loading, data, error } = useQuery(GET_USER, {
+    variables: { id: 11700/* 불러올 아이디 */ },
+  });
+  if (error) {
+    console.log(error);
+  }
+
   if (!loading && data) {
     db_qr_list = data.getUserById.qr_list;
-    db_qr_list.map(w => {
-      item_name.push(w.split('/:')[1]);
+    db_qr_list.map(qr => {
+      item_name.push(qr.split('/:')[1]);
     });
   }
 
@@ -37,12 +37,15 @@ const MyPage = () => {
       <div className='blank'>
         <div className='home__logo'>Collabo</div>
         <div className='point_group'>
-          {!loading && data.getUserById && (<div>{data.getUserById.name}님</div>)}
-
+          {!loading && data && (
+            <div>{data.getUserById.name}님</div>
+          )}
           <div className='point_group_inner'>
             <label className='point__label'>통합 포인트</label>
             <em>
-              {!loading && data.getUserById && (<strong id='point__value'>{data.getUserById.point.toLocaleString()}</strong>)}
+              {!loading && data && (
+                <strong id='point__value'>{data.getUserById.point.toLocaleString()}</strong>
+              )}
               원
             </em>
           </div>
@@ -51,11 +54,11 @@ const MyPage = () => {
 
       <div className='wallet_group'>
         <div className='wallet_inner'>
-          {!loading && data.getUserById && item_name.map((caffe_name) => (
-            <Link to={`/Detail/${caffe_name}`}>
+          {!loading && data && item_name.map((cafe_name, index) => (
+            <Link key={index} to={`/Detail/${cafe_name}`}>
               <div className='wallet__card'>
-                <div className='margin_left'>{caffe_name}</div>
-                <div className='cafe_info'>{cafeInfo}</div>
+                <div className='margin_left'>{cafe_name}</div>
+                <div className='cafe_info'/>
               </div>
             </Link>
           ))}
@@ -64,8 +67,8 @@ const MyPage = () => {
     </div>
   );
 };
-export default MyPage;
 
+export default MyPage;
 
 /*
 const GET_USER = gql`
