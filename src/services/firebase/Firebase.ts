9129fc70ca6938f5e8e89325e84ba10c8cb402fd
KGESH <baram789456@gmail.com>
2021-10-21 20:14:37 +0900

    add dot env js key

--- src/services/firebase/Firebase.ts
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import firebaseConfig from './FirebaseConfig';
-
-const FirebaseInstance = firebase.initializeApp(firebaseConfig.firebase);
-
-export default FirebaseInstance;
--- src/services/kakao/KakaoSdkInit.ts
+++ src/services/kakao/KakaoSdkInit.ts
@@ -1,6 +1,7 @@
 const KakaoSdkInit = () => {
   const { Kakao } = window;
-  const key = 'ebaaf325637b20ae7519bd8d770f8f70';
+  console.log(`env : ${process.env.KAKAO_JS_KEY}`);
+  const key = process.env.KAKAO_JS_KEY || 'ebaaf325637b20ae7519bd8d770f8f70';
   Kakao.init(key);
   if (Kakao?.isInitialized()) {
     console.log(`kakao init `);
